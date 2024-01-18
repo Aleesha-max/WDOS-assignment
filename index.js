877eb213d6fe4a1a57abf169491b825067726640
Aleesha <cb013007@students.apiit.lk>
2024-01-19 00:12:32 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+let images = [["./images/sinharaja1.png", "./images/sinharaja2.jpg"], ["./images/minneriya1.jpeg", "./images/minneriya2.jpg"], ["./images/udawalawe1.jpeg", "./images/udawalawe2.jpeg"]]
+
+
+document.querySelectorAll(".card").forEach( (element, index) => {
+    console.log(element)
+    let array = images[index]
+    let imageIndex=0
+    setInterval(() => {
+        if(imageIndex >array.length-1 ){
+            imageIndex=0
+        }
+        element.querySelector(".img-container").style.backgroundImage=`url(${array[imageIndex]})`
+        imageIndex++
+    }, 4000)
+})
+try { 
+    document.querySelector(".hamburg").addEventListener("click",()=>{
+        document.querySelector(".hamburg-menu").classList.toggle('active')
+        console.log("hfffrf")
+    })
+    
+} catch (error) {
+    
+}
